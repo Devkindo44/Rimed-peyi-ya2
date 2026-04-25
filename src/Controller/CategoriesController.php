f202c805d830b55bbf6a4b5e385713dcd50c45b7
Devkindo44 <ksemadev8@outlook.com>
2026-04-26 01:36:06 +0200

    Premier commit : structure1 rimed peyiya

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/categories')]
+final class CategoriesController extends AbstractController
+
+{
+    #[Route('/', name: 'app_categories')]
+    public function index(): Response
+    {
+        return $this->render('categories/index.html.twig');
+    }
+
+    #[Route('/infusions', name:'app_categories/infusions')]
+    public function infusion(): Response
+
+    {
+        return $this->render('Categories/infusions.html.twig');
+    }
+
+
+    #[Route('/sirops', name:'app_categories/sirops')]
+    public function sirop(): Response
+
+    {
+        return $this->render('Categories/sirops.html.twig');
+    }
+}
